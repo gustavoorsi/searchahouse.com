d4e2ef54c22d0a7a751f3b18e9f5b13eb87cdffb
gustavoorsi <gustavoorsi@gmail.com>
2015-05-12 23:05:53 -0300

    Maven structure for the crud model microservice.

---
+++ searchahouse/src/main/java/edu/searchahouse/SearchahouseApplication.java
@@ -0,0 +1,12 @@
+package edu.searchahouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchahouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchahouseApplication.class, args);
+    }
+}
---
+++ searchahouse/src/test/java/edu/searchahouse/SearchahouseApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.searchahouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SearchahouseApplication.class)
+public class SearchahouseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
