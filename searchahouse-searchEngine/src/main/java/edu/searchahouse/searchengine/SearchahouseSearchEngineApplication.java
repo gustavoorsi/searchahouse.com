c94554e4ae0a900ed0cc9d234eef7ffd95002e2d
gustavoorsi <gustavoorsi@gmail.com>
2015-05-24 19:01:20 -0300

    vanilla :: searchengine microservice
    This is the search engine used to search entities (agents, leads,
    propertyes) on elasticsearch.

---
+++ searchahouse-searchEngine/src/main/java/edu/searchahouse/searchengine/SearchahouseSearchEngineApplication.java
@@ -0,0 +1,12 @@
+package edu.searchahouse.searchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchahouseSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchahouseSearchEngineApplication.class, args);
+    }
+}
