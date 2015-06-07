47b8a0a75b3b5afdf06e86ce9c2a94038cffda87
gustavoorsi <gustavoorsi@gmail.com>
2015-06-07 18:23:27 -0300

    Added basic config for rabbitMQ

---
+++ searchahouse/src/main/java/edu/searchahouse/configuration/RabbitMqConfiguration.java
@@ -0,0 +1,91 @@
+package edu.searchahouse.configuration;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfiguration implements CommandLineRunner{
+	
+	final static String queueName = "SEARCHAHOUSE-QUEUE";
+	
+//	@Autowired
+//	AnnotationConfigApplicationContext context;
+	
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+	
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+	
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("SEARCHAHOUSE-EXCHANGE");
+	}
+	
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(queueName);
+	}
+	
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+	
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+	
+	@Bean
+    Receiver receiver() {
+        return new Receiver();
+    }
+	
+	@Override
+    public void run(String... args) throws Exception {
+        System.out.println("Waiting five seconds...");
+        Thread.sleep(5000);
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(queueName, "Hello from RabbitMQ!");
+        receiver().getLatch().await(10000, TimeUnit.MILLISECONDS);
+//        context.close();
+    }
+
+}
+
+
+class Receiver {
+
+	private CountDownLatch latch = new CountDownLatch(1);
+
+	public void receiveMessage(String message) {
+		System.out.println("Received <" + message + ">");
+		latch.countDown();
+	}
+
+	public CountDownLatch getLatch() {
+		return latch;
+	}
+
+}
