e922330d711bae2182e1bd9f8222e2465f878f3b
gustavoorsi <gustavoorsi@gmail.com>
2015-06-14 15:16:59 -0300

    started project for web microservice.

---
+++ searchahouse-web/src/main/java/edu/searchahouse/web/SearchahouseWebApplication.java
@@ -0,0 +1,12 @@
+package edu.searchahouse.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchahouseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchahouseWebApplication.class, args);
+    }
+}
