b34cb53e5d2474bb64540fc7704ba16dacbde9a6
gustavoorsi <gustavoorsi@gmail.com>
2015-06-22 22:10:44 -0300

    excluded collection loading on agent findById...

--- searchahouse/src/main/java/edu/searchahouse/repository/mongo/AgentRepository.java
+++ searchahouse/src/main/java/edu/searchahouse/repository/mongo/AgentRepository.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import edu.searchahouse.model.Agent;
 
 public interface AgentRepository extends MongoRepository<Agent, ObjectId> {
 
+    @Query(value="{ 'id' : ?0 }", fields="{ 'properties' : 0, 'leads' : 0}")
 	Optional<Agent> findAgentById(final ObjectId id);
 
 	Optional<Agent> findAgentByEmail(final String email);
