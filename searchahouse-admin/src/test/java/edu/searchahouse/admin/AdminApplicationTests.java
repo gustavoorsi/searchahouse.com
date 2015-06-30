24fb029cb1ed1a7191254736be9ca7486b2b6ca2
Gustavo Orsi <gorsi@gorsi-PC.modusagency.com>
2015-06-30 11:15:29 -0300

    Added skeleton packages and web folders.

--- searchahouse-admin/src/test/java/edu/searchahouse/admin/AdminApplicationTests.java
+++ searchahouse-admin/src/test/java/edu/searchahouse/admin/AdminApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = AdminApplication.class)
+@SpringApplicationConfiguration(classes = SearchahouseAdminApplication.class)
 public class AdminApplicationTests {
 
 	@Test
