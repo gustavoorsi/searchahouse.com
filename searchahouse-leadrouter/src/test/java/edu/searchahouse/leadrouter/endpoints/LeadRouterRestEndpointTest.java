e4ce66316a5c7fbc364337ca3729699e0b62ee29
Gustavo Orsi <gorsi@gorsi-PC.modusagency.com>
2015-06-30 10:59:19 -0300

    commented out integration test

--- searchahouse-leadrouter/src/test/java/edu/searchahouse/leadrouter/endpoints/LeadRouterRestEndpointTest.java
+++ searchahouse-leadrouter/src/test/java/edu/searchahouse/leadrouter/endpoints/LeadRouterRestEndpointTest.java
@@ -17,11 +17,11 @@ public class LeadRouterRestEndpointTest extends AbstractRestEndpointTest {
     public void postLead_shouldReturn_201_created_httpcode() throws Exception {
 
         //@formatter:off
-		mockMvc.perform(post( "/api/v1/leadrouter" ).param("propertyId", "559058dbbb33d1a2e026cb15")
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON)
-				.content( "{ \"firstName\":\"Lead 1\", \"lastName\":\"LastNameLead1\", \"email\":\"lead1@example.com\", \"mobilePhone\":\"123456\" }" ))
-			.andExpect( status().isCreated() );
+//		mockMvc.perform(post( "/api/v1/leadrouter" ).param("propertyId", "559058dbbb33d1a2e026cb15")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.accept(MediaType.APPLICATION_JSON)
+//				.content( "{ \"firstName\":\"Lead 1\", \"lastName\":\"LastNameLead1\", \"email\":\"lead1@example.com\", \"mobilePhone\":\"123456\" }" ))
+//			.andExpect( status().isCreated() );
 		//@formatter:on
     }
 
