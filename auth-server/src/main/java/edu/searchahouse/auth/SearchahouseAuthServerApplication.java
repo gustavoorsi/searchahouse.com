f9f7cd880ef8c671636be4adc73a78813f6b3925
gustavoorsi <gustavoorsi@gmail.com>
2015-08-15 16:33:27 -0300

    vanilla project for security

---
+++ auth-server/src/main/java/edu/searchahouse/auth/SearchahouseAuthServerApplication.java
@@ -0,0 +1,12 @@
+package edu.searchahouse.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchahouseAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchahouseAuthServerApplication.class, args);
+    }
+}
